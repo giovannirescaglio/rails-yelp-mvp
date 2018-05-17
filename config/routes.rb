cbeb060fbf0943183fc40ea13b9321156457006c
giovanni rescaglio <giovannirescaglio@gmail.com>
2018-05-17 09:00:19 +0200

    exo done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#read
+# get 'restaurants', to: 'restaurants#index'
+# #create
+# get 'restaurants/new', to: 'restaurants#new'
+# post 'restaurants', to: 'restaurants#create'
+
+# get 'restaurants/:id', to: 'restaurants#show'
+  resources :restaurants, only: [:index, :new, :create, :show] do
+  resources :reviews, only: [ :new, :create, :index]
+  end
 end
+
